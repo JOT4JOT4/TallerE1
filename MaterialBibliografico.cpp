01741ae8f195adf4d4ea398f184eff9ec2200e0a
Tobal29 <130802662+Tobal29@users.noreply.github.com>
2024-09-25 17:15:59 -0400

    Update MaterialBibliografico.cpp
    Implementación del Material Bibliografico.cpp

---
+++ MaterialBibliografico.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "MaterialBibliografico.hpp"
+
+using namespace std;
+
+MaterialBibliografico::MaterialBibliografico(string nombre, int isbn,string autor) {
+
+	this->nombre = nombre;
+	this->isbn =isbn;
+	this->autor=autor;
+	this->prestado=false;
+}
+void MaterialBibliografico::MostrarInformacion() {
+
+	std::cout << nombre<<","<<isbn<<","<<autor<<","<<prestado<< std::endl;
+
+}
+MaterialBibliografico::~MaterialBibliografico() {
+	// Destructor, puede estar vacC-o.
+}
+
