0d4b995031d909ac8b866be41b9800d8a4e1a86e
Tobal29 <130802662+Tobal29@users.noreply.github.com>
2024-09-25 17:15:10 -0400

    Update Libro.cpp
    creación de Libro.cpp

---
+++ Libro.cpp
@@ -0,0 +1,20 @@
+#include "Libro.hpp"
+#include <iostream>
+
+Libro::Libro(string nombre,int isbn,string autor,string fechaP,string resumen)
+	:MaterialBibliografico(nombre,isbn,autor)
+{
+	this->fechaP = fechaP;
+	this->resumen= resumen;
+}
+
+void  Libro::MostrarInformacion() {
+
+	MaterialBibliografico::MostrarInformacion();
+	std::cout << fechaP<<","<<resumen << std::endl;
+
+}
+
+Libro::~Libro() {
+
+}
