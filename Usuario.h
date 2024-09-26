587c026c16620d24e6ec34069f08130d1011095b
Tobal29 <130802662+Tobal29@users.noreply.github.com>
2024-09-26 12:10:11 -0300

    Update Usuario.h
    se agrego un getNombre y completaron los metodos

--- Usuario.h
+++ Usuario.h
@@ -1,21 +1,24 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "MaterialBibliografico.h"
+#include "MaterialBibliografico.hpp"
 
 using namespace std;
 
 class Usuario
 {
 private:
-    /* data */
+    
     string nombre;
     int id;
     MaterialBibliografico* materialesPrestados[5];
+    
 public:
-    Usuario(/* args */);
-    void prestarMaterial();
-    void devolverMaterial();
+
+    Usuario(string nombre,int id);
+    void prestarMaterial(MaterialBibliografico* material);
+    MaterialBibliografico* devolverMaterial(string nombreB);
     void mostrarMaterialesPrestados();
+    string getNombre();
     ~Usuario();
 };
